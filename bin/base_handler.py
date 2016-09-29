fbd98d2c23385d051ebe2dfea2ca3ffe97647f9c
Robert <->
2016-09-29 20:59:25 +0200

    Added utility method to call json based rest endpoints

--- bin/base_handler.py
+++ bin/base_handler.py
@@ -2,13 +2,20 @@ from splunk import auth, search
 import splunk.rest
 import logging as logger
 import splunk.bundle as bundle
-import httplib2, urllib, os, time
+import httplib2, urllib, urllib2, os, time
 import json
 import splunklib.client as client
 from splunklib.binding import _spliturl as spliturl
 from splunklib.binding import namespace as namespace
 import base64
 import random
+
+
+class Request(urllib2.Request):
+    def set_method(self,method):
+        self.__method=method
+    def get_method(self):
+        return self.__method
 
 class BaseRestHandler(splunk.rest.BaseRestHandler):
     def create_service(self):
@@ -26,3 +33,15 @@ class BaseRestHandler(splunk.rest.BaseRestHandler):
             host=host)
         s.login()
         return s
+    def call_json_service(self,method,path,payload):
+        management_url = "https://"+self.request["headers"]["host"]+path
+        req = Request(management_url, json.dumps(payload), {
+            "Content-Type": "application/json",
+            "Authorization": self.request["headers"].get("authorization", ""),
+        })
+        req.set_method(method)
+        res = urllib2.urlopen(req)
+        if res.code!=200:
+            raise Exception('response code %s' % res.code)
+        body = res.read()
+        return json.loads(body)
