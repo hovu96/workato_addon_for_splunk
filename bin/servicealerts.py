878074d3fcf35582e8584dfb9fe8e5fdd0378b05
Robert <->
2016-09-30 20:35:05 +0200

    Added support for subscribing for IT service alerts

---
+++ bin/servicealerts.py
@@ -0,0 +1,53 @@
+import json
+from .base_handler import BaseRestHandler
+from .utils import workato_app_name
+from .alert_action_utils import has_callback
+
+service_alert_name = 'IT Service Alerts'
+
+class ServiceAlertsHandler(BaseRestHandler):
+    def get_search(self):
+        s = self.create_service()
+        saved_searches = s.saved_searches
+        search = saved_searches[service_alert_name]
+        return search
+    def handle_GET(self):
+        search = self.get_search()
+        subscribed = has_callback(search)
+        self.response.setStatus(200)
+        self.response.setHeader('content-type', 'application/json')
+        self.response.write(json.dumps({
+            "subscribed": subscribed,
+            "is_scheduled": search.is_scheduled,
+            "disabled": search.disabled,
+        }))
+    def handle_POST(self):
+        payload = json.loads(self.request['payload'])
+         subscribe_response = self.call_json_service(
+            "POST",
+            "/services/workato/scheduledsearches",
+            {
+            "callback_url": payload["callback_url"],
+            "search_name": service_alert_name
+            }
+        )
+        search = self.get_search()
+        search.enable()
+        self.response.setStatus(200)
+        self.response.setHeader('content-type', 'application/json')
+        self.response.write(json.dumps(subscribe_response))
+    def handle_DELETE(self):
+        payload = json.loads(self.request['payload'])
+        unsubscribe_response = self.call_json_service(
+            "DELETE",
+            "/services/workato/scheduledsearches",
+            {
+            "callback_url": payload["callback_url"],
+            "search_name": service_alert_name
+            }
+        )
+        search = self.get_search()
+        search.disable()
+        self.response.setStatus(200)
+        self.response.setHeader('content-type', 'application/json')
+        self.response.write(json.dumps(unsubscribe_response))
