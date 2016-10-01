6177e8f583821aba82b0bbd76b6c0b3f8e27aa32
Robert <->
2016-10-01 13:28:20 +0200

    Added version numbers to /version REST endpoint

--- bin/version.py
+++ bin/version.py
@@ -1,12 +1,13 @@
 import json
 from .base_handler import BaseRestHandler
+from .utils import workato_app_name
 
 class VersionHandler(BaseRestHandler):
     def handle_GET(self):
         s = self.create_service()
         self.send_json_response({
-            "splunk_version": "",
-            "itsi_version": "",
-            "es_version": "",
-            "addon_version": "",
+            "splunk_version": s.info["version"],
+            "itsi_version": s.apps['itsi'].version if 'itsi' in s.apps else "",
+            "es_version": s.apps['SplunkEnterpriseSecuritySuite'].version if 'SplunkEnterpriseSecuritySuite' in s.apps else "",
+            "workato_version": s.apps[workato_app_name].version if workato_app_name in s.apps else "",
         })
