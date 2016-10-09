e65e0f021e2841f590b5d284848311ca2e2eb9ca
Robert <->
2016-10-09 14:57:39 +0200

    Improved alert action logging

--- bin/workato_alert_action.py
+++ bin/workato_alert_action.py
@@ -8,36 +8,53 @@ from alert_action_utils import iterate_callbacks_from_string
 def call_workato(payload):
 
     config = payload.get('configuration')
-
     callback_urls = config.get('callback_urls')
     if callback_urls is None:
         callback_urls = ""
     callback_urls = list(iterate_callbacks_from_string(callback_urls))
 
-    results_file = payload.get('results_file')
+    sid = payload.get('sid')
+    def log(level,msg):
+        print >> sys.stderr, "%s sid=\"%s\" %s" % (level,sid,msg)
+    def log_info(msg):
+        log("INFO",msg)
+    def log_debug(msg):
+        log("DEBUG",msg)
+    def log_error(msg):
+        log("ERROR",msg)
 
+    alert_count = 0
+    callback_url_count = len(callback_urls)
+    callback_invocation_attempts = 0
+    callback_invocation_errors = 0
+
+    results_file = payload.get('results_file')
     with gzip.open(results_file, 'rb') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
+            alert_count += 1
             payload = {}
             for name, value in row.iteritems():
                 if not name.startswith('__mv'):
                     payload[name]=value
             for callback_url in callback_urls:
-                #print >> sys.stderr, "DEBUG sending %s ..." % payload
-                req = urllib2.Request(callback_url, json.dumps(payload), {"Content-Type": "application/json"})
-                res = urllib2.urlopen(req)
-                body = res.read()
-                print >> sys.stderr, "DEBUG server responded with HTTP status=%d" % res.code
-                #print >> sys.stderr, "DEBUG server response: %s" % json.dumps(body)
+                callback_invocation_attempts += 1
+                try:
+                    req = urllib2.Request(callback_url, json.dumps(payload), {"Content-Type": "application/json"})
+                    res = urllib2.urlopen(req)
+                    body_bytes = len(str(res.read()))
+                    log_info("callback=\"%s\" status=%d body_length=\"%d\"" % (callback_url, res.code, body_bytes))
+                except urllib2.HTTPError, e:
+                    callback_invocation_errors += 1
+                    log_error("callback=\"%s\" error=\"%s\"" % (callback_url, str(e)))
+
+    log_info("alert_count=%d callback_url_count=%d callback_invocation_attempts=%d callback_invocation_errors=%d" % (alert_count, callback_url_count, callback_invocation_attempts, callback_invocation_errors))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--execute":
         payload = sys.stdin.read()
-        print >> sys.stderr, "INFO " + payload
         payload = json.loads(payload)
         call_workato(payload)
-        print >> sys.stderr, "INFO done"
     else:
         print >> sys.stderr, "FATAL Unsupported execution mode (expected --execute flag)"
         sys.exit(1)
