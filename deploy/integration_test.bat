7d5383d5f30550c80f25d53b285748d62d71e19e
Robert Fujara <robert.fujara@gmail.com>
2017-05-26 10:22:45 +0200

    Tests now run on Splunk 6.6.0

--- deploy/integration_test.bat
+++ deploy/integration_test.bat
@@ -15,7 +15,7 @@ docker rm -fv workato_test
 set NETWORK_NAME="workato-integration-test"
 docker network create %NETWORK_NAME%
 
-docker run -p 8000:8000 --name workato_splunk -d -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_CMD=edit user admin -password admin -auth admin:changeme" -v %DEPOY_PATH%/../:/opt/splunk/etc/apps/workato_addon_for_splunk:rw -v %DEPOY_PATH%/test_app:/opt/splunk/etc/apps/test_app:rw --network %NETWORK_NAME% splunk/splunk:latest
+docker run -p 8000:8000 --name workato_splunk -d -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_CMD=edit user admin -password admin -auth admin:changeme" -v %DEPOY_PATH%/../:/opt/splunk/etc/apps/workato_addon_for_splunk:rw -v %DEPOY_PATH%/test_app:/opt/splunk/etc/apps/test_app:rw --network %NETWORK_NAME% splunk/splunk:6.6.0
 
 docker run --name workato_test -it --rm -v %DEPOY_PATH%/test:/usr/src/myapp:ro -w /usr/src/myapp --network %NETWORK_NAME% python:2.7 python2.7 integration_test.py workato_splunk 8089 workato_test
 
